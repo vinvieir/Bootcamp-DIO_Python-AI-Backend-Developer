d84865bd329402878e5e0bd48268b302d828d88e
pblvvs <pblvvs16@icloud.com>
2024-05-16 11:38:35 -0300

    first-commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Registro dos meus primeiros passos em Python
+
+Criei essa pasta com o objetivo de deixar registrado todos os arquivos e códigos criados durante o meu processo realizando o Bootcamp da DIO: Python AI Backend Developer, que é meu primeiro contato com Python
---
+++ Tipos de Operadores com Python/operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+produto_1 = 10
+produto_2 = 29
+produto_3 = 14
+produto_4 = 31
+
+print(produto_1 + produto_2)
+
+#Resultado não fica negativo
+print(produto_2 - produto_1)
+
+print(produto_1 * produto_3)
+
+#O poder de um asterisco
+print(produto_1 ** produto_3)
+
+print(produto_4 / produto_1)
+
+print(produto_4 // produto_1)
