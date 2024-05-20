61e363585c94d193944dd5ed22ffaa79af90461c
pblvvs <pblvvs16@icloud.com>
2024-05-20 10:53:04 -0300

    tipos de operadores

---
+++ Tipos de Operadores com Python/operadores_atribuicao.py
@@ -0,0 +1,40 @@
+saldo = 500
+
+print(saldo)
+
+saldo = 200
+
+print(saldo)
+
+saldo += 30
+
+print(saldo)
+
+saldo -= 10
+
+print(saldo)
+
+saldo //= 12
+
+print(saldo)
+
+saldo /= 2
+
+print(saldo)
+
+#Após fazer uma divisão mesmo botando int o resultado ainda apresenta ".0" no final.
+saldo //= 2
+
+print(saldo)
+
+saldo *= 5
+
+print(saldo)
+
+saldo **= 3
+
+print(saldo)
+
+saldo %= 6
+
+print(saldo)
